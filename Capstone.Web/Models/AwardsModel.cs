bb01ceacc0b7ea8d05a03e0421c69d7f262720cc
Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2016-11-30 13:45:03 -0500

    updates to capstone

---
+++ Capstone.Web/Models/AwardsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class AwardsModel
+    {
+        public string Category { get; set; }
+        public string ListOfTitles { get;  set;}
+        public string ListOfCharacters { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
