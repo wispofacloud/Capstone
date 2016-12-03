937588d2169e3666bac5bd0965a67a49d7579dfa
Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2016-12-03 17:32:34 -0500

    Test Structure

--- Capstone.Web.Tests/DALTests/ReadingListSqlDALTest.cs
+++ Capstone.Web.Tests/DALTests/ReadingListSqlDALTest.cs
@@ -1,14 +1,53 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Transactions;
+using Capstone.Web.Models;
+using System.Collections.Generic;
+
 
 namespace Capstone.Web.Tests.DALTests
 {
     [TestClass]
     public class ReadingListSqlDALTest
     {
+        private TransactionScope tran;
+        private string connectionString = ConfigurationManager.ConnectionStrings["EchoBooks"].ConnectionString;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            tran = new TransactionScope();
+
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+            }
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            tran.Dispose();
+        }
+
+  
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddBookToReadingList_ByBookID()
         {
         }
+
+        [TestMethod]
+        public void TestChangeBookToHasRead()
+        {
+        }
+
+        [TestMethod]
+        public void TestGetReadingList()
+        {
+
+        }
+
     }
 }
