eed84350bb0c1b06788983423eba03b1ea8cf0fa
Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2016-12-05 14:37:24 -0500

    Updated Capstone

---
+++ Capstone.Web.Tests/DALTests/ForumSqlDALTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capstone.Web.Tests.DALTests
+{
+    [TestClass]
+    public class ForumSqlDALTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
