fc2ceb801ad04c1f9c047bbbf9fb87465dd1add9
Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2016-12-05 13:19:45 -0500

    Updated Capstone

---
+++ Capstone.Web/Models/CategoriesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class CategoriesModel
+    {
+        public string RecommendationsNeeded { get; set; }
+        public string PlotReviews { get; set; }
+        public string FanFiction { get; set; }
+    }
+}
---
+++ Capstone.Web/Models/ThreadsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class ThreadsModel
+    {
+        public int UserID { get; set; }
+        public int ThreadID { get; set; }
+        public string Title { get; set; }
+    }
+}
