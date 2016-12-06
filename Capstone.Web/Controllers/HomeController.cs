8d9a5d04928fdd1e97f1878aa4089c3a02eb34d2
Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2016-12-06 17:04:55 -0500

    partialview new author list

--- Capstone.Web/Controllers/HomeController.cs
+++ Capstone.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,25 @@ namespace Capstone.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IBooksDAL booksDAL;
+
+        public HomeController(IBooksDAL booksDAL)
+        {
+            this.booksDAL = booksDAL;
+        }
 
         // GET: Home
         public ActionResult Index()
         {
             return View("Index");
+        }
+
+        //Get: Get New Author List
+        public ActionResult RenderPartialNewAuthorList()
+        {
+            List<String> model = new List<String>();
+            model = booksDAL.GetNewAuthorList();
+            return View("_PartialNewAuthorList", model);
         }
     }
 }
