3c37c4e29e97f233b66c8fb2cd003248012e72be
Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2016-12-07 11:39:35 -0500

    tests

--- Capstone.Web.Tests/DALTests/ForumSqlDALTest.cs
+++ Capstone.Web.Tests/DALTests/ForumSqlDALTest.cs
@@ -1,14 +1,67 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Transactions;
+using System.Data.SqlClient;
+using System.Configuration;
+using Capstone.Web.Models;
+using System.Collections.Generic;
+using Capstone.Web.DAL;
+
+
 
 namespace Capstone.Web.Tests.DALTests
 {
     [TestClass]
     public class ForumSqlDALTest
     {
+        private TransactionScope tran;
+        private string connectionString = ConfigurationManager.ConnectionStrings["EchoBooks"].ConnectionString;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            tran = new TransactionScope();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+               // SqlCommand cmd = new SqlCommand(, conn);
+              //  cmd.ExecuteNonQuery();
+               // SqlCommand cmd2 = new SqlCommand(, conn);
+            }
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            tran.Dispose();
+        }
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetAllPostsByThreadID()
         {
         }
+        [TestMethod]
+        public void TestGetThreadsByCategory_ByCategoryID()
+        {
+        }
+        [TestMethod]
+        public void TestGetThreadByThreadID()
+        {
+        }
+        [TestMethod]
+        public void TestSubmitPost()
+        {
+        }
+        [TestMethod]
+        public void TestSubmitReview()
+        {
+        }
+        [TestMethod]
+        public void TestGetAllCategories()
+        {
+        }
+
+
+
+
+
     }
 }
